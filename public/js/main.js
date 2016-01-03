90c2e5b333a0e142a39dced5f8a4adb8b46f4bff
tca85 <thiago.alves.85@gmail.com>
2016-01-02 19:17:43 -0500

    transformando em Single Page Application utilizando rotas

--- public/js/main.js
+++ public/js/main.js
@@ -2,8 +2,33 @@
 // módulo principal da aplicação.
 // a função module recebe dois parâmetros:
 // O primeiro é o nome do módulo que desejamos criar e 
-// o segundo éma array com todos os módulos de que nosso módulo depende. 
-// Como não avançamos ainda com o projeto, não temos nenhuma dependência ainda, 
-// porém, você não deve omitir este parâtro.
+// o segundo é um array com todos os módulos de que nosso módulo depende. 
+// Mesmo que não haja dependências, esse parâmetro não pode ser omitido
 
-angular.module('alurapic', ['minhasDiretivas', 'ngAnimate']);
+angular.module('alurapic', ['minhasDiretivas', 'ngAnimate', 'ngRoute'])
+
+//  Para configurar o sistema de rotas, ngRoute é pré-requisito
+//  para que possa receber o $routeProvider por injeção de dependências
+//  Por padrão, a url precisa ter '#' antes do nome da rota
+//  $locationProvider - ativa a utilização do HTML5
+    .config(function($routeProvider, $locationProvider){
+
+    // habilita a utilização de HMTL5, para que não precise colocar # na URL
+    // precisar colocar <base href="/"> na página inicial
+    $locationProvider.html5Mode(true);
+
+    // http://localhost:3000/fotos
+    $routeProvider.when('/fotos', {
+        templateUrl: 'partials/principal.html',
+        controller: 'FotosController'
+    });
+
+    // http://localhost:3000/fotos/new
+    $routeProvider.when('/fotos/new', {
+        templateUrl: 'partials/foto.html' 
+    });
+
+    // se o usuário informar uma rota que não existe, vai para um default
+    $routeProvider.otherwise({ redirectTo: '/fotos'});
+
+});
